ba00bd1653b63d6fff452339c23856071aac6cee
Desmond Coacher <dcoacher@outlook.com>
2025-11-22 20:06:24 +0200

    Updated incorrect data in outputs.tf

--- iac/terraform/outputs.tf
+++ iac/terraform/outputs.tf
@@ -2,35 +2,35 @@
 
 output "control_plane_ip" {
   description = "Control Plane Public IP"
-  value       = aws_instance.control_plane.public_ip
+  value       = aws_instance.CONTROL-PLANE.public_ip
 }
 
 output "worker_1_ip" {
   description = "Worker 1 Public IP"
-  value       = aws_instance.worker_1.public_ip
+  value       = aws_instance.WORKER-1.public_ip
 }
 
 output "worker_2_ip" {
   description = "Worker 2 Public IP"
-  value       = aws_instance.worker_2.public_ip
+  value       = aws_instance.WORKER-2.public_ip
 }
 
 output "alb_dns_name" {
   description = "ALB DNS Name"
-  value       = aws_lb.main.dns_name
+  value       = aws_lb.ITAM-ALB.dns_name
 }
 
 output "control_plane_private_ip" {
   description = "Control Plane Private IP"
-  value       = aws_instance.control_plane.private_ip
+  value       = aws_instance.CONTROL-PLANE.private_ip
 }
 
 output "worker_1_private_ip" {
   description = "Worker 1 Private IP"
-  value       = aws_instance.worker_1.private_ip
+  value       = aws_instance.WORKER-1.private_ip
 }
 
 output "worker_2_private_ip" {
   description = "Worker 2 Private IP"
-  value       = aws_instance.worker_2.private_ip
+  value       = aws_instance.WORKER-2.private_ip
 }
