4d78c5a7bd42bde2f47aba64591d5e7a69460168
Desmond Coacher <dcoacher@outlook.com>
2025-11-22 20:43:36 +0200

    Resolved incorrect values routes between .tf files

--- iac/terraform/outputs.tf
+++ iac/terraform/outputs.tf
@@ -1,36 +1,36 @@
 # Outputs
 
-output "control_plane_ip" {
+output "CONTROL-PLANE-PUBLIC-IP" {
   description = "Control Plane Public IP"
   value       = aws_instance.CONTROL-PLANE.public_ip
 }
 
-output "worker_1_ip" {
+output "WORKER-1-PUBLIC-IP" {
   description = "Worker 1 Public IP"
   value       = aws_instance.WORKER-1.public_ip
 }
 
-output "worker_2_ip" {
+output "WORKER-2-PUBLIC-IP" {
   description = "Worker 2 Public IP"
   value       = aws_instance.WORKER-2.public_ip
 }
 
-output "alb_dns_name" {
+output "ALB-DNS-NAME" {
   description = "ALB DNS Name"
   value       = aws_lb.ITAM-ALB.dns_name
 }
 
-output "control_plane_private_ip" {
+output "CONTROL-PLANE-PRIVATE-IP" {
   description = "Control Plane Private IP"
   value       = aws_instance.CONTROL-PLANE.private_ip
 }
 
-output "worker_1_private_ip" {
+output "WORKER-1-PRIVATE-IP" {
   description = "Worker 1 Private IP"
   value       = aws_instance.WORKER-1.private_ip
 }
 
-output "worker_2_private_ip" {
+output "WORKER-2-PRIVATE-IP" {
   description = "Worker 2 Private IP"
   value       = aws_instance.WORKER-2.private_ip
 }
