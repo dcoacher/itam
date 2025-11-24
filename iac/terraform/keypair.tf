a382316236becd0075dca775eda740b78c50491c
Desmond Coacher <dcoacher@outlook.com>
2025-11-24 23:00:57 +0200

    Update keypair.tf

--- iac/terraform/keypair.tf
+++ iac/terraform/keypair.tf
@@ -23,3 +23,40 @@ resource "aws_key_pair" "ITAM-KP" {
     Name = "itam-keypair"
   }
 }
+
+# KP Copying from Terraform to Ansible Control VM
+resource "terraform_data" "Key_Pair_Copy" {
+  # Recreate this resource (and rerun provisioners) whenever the local key changes
+  triggers_replace = {
+    key_checksum = sha256(file("${path.module}/KP.pem"))
+  }
+
+  # One connection for all provisioners
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = tls_private_key.ITAM-KP.private_key_pem
+    host        = aws_instance.CONTROL-PLANE.public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "mkdir -p /home/ubuntu/",
+      "chmod 700 /home/ubuntu/",
+      "chown ubuntu:ubuntu /home/ubuntu/"
+    ]
+  }
+
+  provisioner "file" {
+    source      = "${path.module}/KP.pem"
+    destination = "/home/ubuntu/KP.pem"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod 600 /home/ubuntu/KP.pem",
+      "chown ubuntu:ubuntu /home/ubuntu/KP.pem"
+    ]
+  }
+
+}
